cb0805888ec933dd43f85e74c70d8cd170e188b2
Micah Ferrill <mcferrill@gmail.com>
2021-11-05 20:16:57 -0500

    Using issue branch to test updates.

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <html>
+    <head>
+      <title>Bar Calculator</title>
+    </head>
+    <body class="bg-gray-900 text-gray-100 h-screen flex flex-col">
+      <Nuxt />
+    </body>
+  </html>
+</template>
