3b4a49571e9272fef04cdd0efc6b54d25f84088c
Micah Ferrill <mcferrill@gmail.com>
2021-11-05 19:58:49 -0500

    Working on screen height issue.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-900 text-gray-100 h-screen">
+  <div class="bg-gray-900 text-gray-100 h-screen flex flex-col">
     <form id="plate_form">
       <div class="flex-none md:flex">
         <div class="flex-none w-full md:w-1/3 md:flex-1 px-5 pt-2 h-20">
