f6bbeaf9a9f5db41a26ded6e9a6cbdc49287451b
Micah Ferrill <mcferrill@pm.me>
2025-03-22 15:36:23 -0500

    Moved sentry into bun config.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
 import React from 'react';
 import Calculator from "./Calculator";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+	dsn: "https://a2c11aa7a21741df93d4a04cc87fa8e7@o4509023277023232.ingest.us.sentry.io/4509023278727168"
+});
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root container not found");
