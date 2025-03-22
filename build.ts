28a0c3657d7b5192b0f5089e297b62ceaee1b4ab
Micah Ferrill <mcferrill@pm.me>
2025-03-21 21:13:58 -0500

    Rewrote scripts to TS.

--- build.ts
+++ build.ts
@@ -2,11 +2,11 @@ import { file, build, write } from "bun";
 import tailwindPlugin from "bun-plugin-tailwind";
 
 const f = file("src/index.html");
-await write("dist/index.html", f);
+write("dist/index.html", f);
 
-await build({
+build({
     entrypoints: ["./src/index.tsx"],
     outdir: "./dist",
     plugins: [tailwindPlugin],
     target: "browser",
-}).catch((e) => console.error(e));
+});
